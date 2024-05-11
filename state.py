5de50e755486b419f4c8fe3e3a007d7bf53a42ce
“krv006” <rvkamronbek@gmail.com>
2024-05-11 22:11:51 +0500

    new buttons

--- state.py
+++ state.py
@@ -3,23 +3,18 @@ from aiogram.fsm.state import State, StatesGroup
 
 class PartnerState(StatesGroup):
     full_name = State()
-    age = State()
+    age = State() # {sherik kerakda va xodim kerakda yoq}  (shogird, ustoz, ish joyi)
     texnology = State()
+    telegram = State()
     phone_number = State()
     location = State()
-    price = State()
-    job = State()
+    masul = State()
+    price = State() # xodm kerakda
+    job = State() # xodm kerakda
+    murojat_qilish = State
+    maosh = State() # xodm kerakda
+    qoshimcha = State() # xodm kerakda
     time = State()
     goal = State()
 
 
-class WorkState(StatesGroup):
-    office_name = State()
-    texnology = State()
-    phone_number = State()
-    location = State()
-    responsible_person = State()
-    time = State()
-    time_worked = State()
-    work_price = State()
-    informations = State()
