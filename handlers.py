6512fe0b9386444b98e32a3046c7ef6c3e7116aa
“krv006” <rvkamronbek@gmail.com>
2024-05-20 16:45:03 +0500

    gitignore

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, InlineKeyboardButton, CallbackQuery, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import Message, InlineKeyboardButton, CallbackQuery, KeyboardButton
 from aiogram.utils.i18n import gettext as _
 from aiogram.utils.i18n import lazy_gettext as __
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
